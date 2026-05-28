d4bfeab366f5105a9a02f35e3eab5578f579599d
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-05-28 15:21:47 +0530

    files added

--- shell-scripting/08-functions-1.sh
+++ shell-scripting/08-functions-1.sh
@@ -15,26 +15,26 @@ fi
 VALIDATE() {
     if [ $1 -ne 0 ] # $? exit codes staus store/reveives on this $1 or passing exit code in $1
     then
-        echo "$2 ... FAILED" &>>LOG_FILE
+        echo "$2 ... FAILED" &>>$LOG_FILE
         exit 1  # it'll stop here
     else   
-        echo "$2 ... SUCCESS" &>>LOG_FILE
+        echo "$2 ... SUCCESS" &>>$LOG_FILE
     fi    
 }
 
-apt install git -y &>>LOG_FILE
+apt install git -y &>>$LOG_FILE
 
 VALIDATE $? "GIT Installation"  ## $2 exit status of above function/cmd
 
-apt install vimmmm -y &>>LOG_FILE
+apt install vimmmm -y &>>$LOG_FILE
 
 VALIDATE $? "VIM Installation"
 
-apt install wget -y &>>LOG_FILE
+apt install wget -y &>>$LOG_FILE
 
 VALIDATE $? "WGET Installation"
 
-apt install net-tools -y &>>LOG_FILE
+apt install net-tools -y &>>$LOG_FILE
 
 VALIDATE $? "NET-TOOLS"
 
