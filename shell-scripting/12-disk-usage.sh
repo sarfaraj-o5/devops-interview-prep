9343ae01eaf31028a47cba509b1e4a6b579732cb
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-05-30 09:11:44 +0530

    files added

--- shell-scripting/12-disk-usage.sh
+++ shell-scripting/12-disk-usage.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
 # df -hT |grep -vE 'tmpfs|Filesystem' | awk '{print $6 " " $1}' 
-
 # df -hT |grep -vE 'tmpfs|Filesystem' | awk '{print $6 " " $1}' | cut -d "%" -f1
+# df -hT |grep -vE 'tmpfs|Filesystem' | awk '{print $6 " " $1}' | cut -d " " -f2
 
-DISK_THRESHOLD=10
+DISK_THRESHOLD=1
 DISK_USAGE=$(df -hT |grep -vE 'tmpfs|Filesystem' | awk '{print $6 " " $1}')
+message=""
 
 while IFS= read line;
 do 
     usage=$(echo $line | cut -d "%" -f1)
+    partition=$(echo $DISK_USAGE | cut -d " " -f2)
     echo "usage: $usage"
+    echo "usage: $partition"
+    if [ $usage -ge $DISK_THRESHOLD ]
+    then
+        message+= "High Disk usage on $partition: $usage%\n"
+    fi 
 done <<< "$DISK_USAGE"
+
+echo "message: $message"
