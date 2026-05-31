b9f3a7a5d4be0a667dd7ed805d1a2cc849583a9a
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-05-31 08:23:53 +0530

    files added

--- shell-scripting/mail.sh
+++ shell-scripting/mail.sh
@@ -4,4 +4,9 @@ TO=#1
 SUBJECT=$2
 BODY_CONTENT=$3
 NAME=$4
-AL
+ALERT_TYPE=$2
+template=""
+
+final_content=$(sed -e "s/TEAM/$NAME/g" -e "s/BODY_CONTENT/$BODY_CONTENT/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" "$template")
+
+echo "final_content: $final_content"
