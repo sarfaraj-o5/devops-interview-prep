1a37a71cd6e91ccb068df038b97522e25961fa21
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-05-31 08:25:12 +0530

    files added

--- shell-scripting/mail.sh
+++ shell-scripting/mail.sh
@@ -5,7 +5,7 @@ SUBJECT=$2
 BODY_CONTENT=$3
 NAME=$4
 ALERT_TYPE=$2
-template=""
+template="/root/devops-interview-prep/shell-scripting/template/template.html"
 
 final_content=$(sed -e "s/TEAM/$NAME/g" -e "s/BODY_CONTENT/$BODY_CONTENT/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" "$template")
 
