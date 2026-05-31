d1dc298298f16724015692f4d31a5fde4657a5c7
sarfaraj-o5 <naikwadisarfaraj@gmail.com>
2026-05-31 09:08:02 +0530

    files added

--- shell-scripting/12-disk-usage.sh
+++ shell-scripting/12-disk-usage.sh
@@ -22,7 +22,7 @@ done <<< "$DISK_USAGE"
 
 echo "message: $message"
 
-echo -e "$message" | mail -s "High Disk Usage" info@example.com  ## -e = enable backslash special chars
+# echo -e "$message" | mail -s "High Disk Usage" info@example.com  ## -e = enable backslash special chars
 ##    msg body              Subject             email-addres  
 
 bash mail.sh abc@example.com "High Disk Usage" "$message" "DevOps Team"
